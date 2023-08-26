b0da274200999c6df3dc09c134dc17aaa6131397
Ziad Reda <83479088+ziadreda72@users.noreply.github.com>
2023-08-26 09:59:30 +0300

    Create Bipartite Graph Check.cpp

---
+++ Graphs/Bipartite Graph Check.cpp
@@ -0,0 +1,22 @@
+vector < int > color(n + 1 , -1);
+        bool is_bipartite = true;
+        queue < int > q;
+        for(int i = 1 ; i <= n ; ++i){
+                if(color[i] == -1){
+                        q.push(i);
+                        color[i] = 0;
+                        while(!q.empty()){
+                                int u = q.front();
+                                q.pop();
+                                for(int v : g[u]){
+                                        if(color[v] == -1){
+                                                color[v] = color[u] ^ 1;
+                                                q.push(v);
+                                        }
+                                        else{
+                                                is_bipartite &= color[v] != color[u];
+                                        }
+                                }
+                        }
+                }
+        }
