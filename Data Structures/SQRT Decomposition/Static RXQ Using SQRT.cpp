91c021f34b45c3b485e039a922ef5e2745ce4252
Ziad Reda <83479088+ziadreda72@users.noreply.github.com>
2023-09-15 16:48:54 +0300

    Create Static RXQ Using SQRT.cpp

---
+++ Data Structures/SQRT Decomposition/Static RXQ Using SQRT.cpp
@@ -0,0 +1,50 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int SQRT(int x){
+        int lo = 0 , hi = 1;
+        while(hi * hi <= x) hi <<= 1;
+
+        while(hi - lo > 1){
+                int mid = (lo + hi) >> 1;
+                if(mid * mid >= x){
+                        hi = mid;
+                }else{
+                        lo = mid;
+                }
+        }
+
+        return hi;
+}
+
+int32_t main(){
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int n , q;
+        cin >> n >> q;
+
+        int B = SQRT(n);
+        vector < int > a(n) , b(B , 0);
+        for(int i = 0 ; i < n ; ++i){
+                cin >> a[i];
+                b[i / B] ^= a[i];
+        }
+
+        while(q--){
+                int l , r;
+                cin >> l >> r;
+                --l , --r;
+                int ans = 0;
+                for(int i = l ; i <= r ;){
+                        if(i % B == 0 && i + B - 1 <= r){
+                                ans ^= b[i / B];
+                                i += B;
+                        }else{
+                                ans ^= a[i++];
+                        }
+                }
+                cout << ans << '\n';
+        }
+}
